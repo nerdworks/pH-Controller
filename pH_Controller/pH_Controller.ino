5c957d80953bc395fadd9ae1429f65e11fe17f72
nerdworks <jk@nerdworks.org>
2014-04-09 07:04:52 +0200

    Adding details

--- pH_Controller/pH_Controller.ino
+++ pH_Controller/pH_Controller.ino
@@ -16,7 +16,7 @@
 
 #define rxpin 2                                       //set the RX pin to pin 2
 #define txpin 3                                       //set the TX pin to pin 3
-
+//#define rssipin ?                                   // Read RSSI signal, and display bars on LCD
 
 int pHSet = 650;                                      // Legg inn settpunkt 100 ghanger større enn ønsket pH.
 
@@ -37,6 +37,15 @@ byte second;
 String checkProbeString = "check probe";
 static String alarm = "";
 
+static int nowYear;
+static int nowMonth;
+static int nowDay;
+static int nowHour;
+static int nowMinute;
+static int nowSecond;
+
+static unsigned long currentMillis;
+static unsigned long lastCurrentMillis;
 
 void setup() {
   // set up the LCD's number of rows and columns: 
@@ -56,6 +65,8 @@ Serial.println("RTC is NOT running!");
      myserial.begin(38400);                                                    //set baud rate for software serial port to 38400
      inputstring.reserve(5);                                                   //set aside some bytes for receiving data from the PC
      sensorstring.reserve(30);                                                 //set aside some bytes for receiving data from Atlas Scientific product
+     currentMillis = millis();                                                 //
+     lastCurrentMillis = currentMillis;
 
   
 } //End Setup
@@ -64,6 +75,7 @@ Serial.println("RTC is NOT running!");
                char inchar = (char)Serial.read();                               //get the char we just received
                inputstring += inchar;                                           //add it to the inputString
                if(inchar == '\r') {input_stringcomplete = true;}                //if the incoming character is a <CR>, set the flag
+               
      } 
 
 void loop() {
@@ -73,20 +85,67 @@ void loop() {
   Når check probe eller annen alarm, f.eks temp probe, clear display og blink display
   
   fjern delay()
-  Les pH 10 ganger i sekundet og midle.
-  Send temp til pH dings 1 gang i sekundet
+  
   
   Bruk millis for å bestemme når pH skal leses, display oppdateres, konsoll oppdateres osv
   millis og %-funksjon...
   
   bruk heltall for behandling av pH inntil verdien skal ut til f.eks display osv
   
-  Lag rutine for hvor langt ute kalibreringen er, slik at vi kan si om proben bør skiftes
-  
-  
-  
-  
   */
+ 
+ 
+ // 1. Se om det noe innkommende å håndtere fra knapper eller serial, og håndter det som er kommet inn
+ //        1.1 Still klokka
+ //        1.2 Endre pH settpunkt
+ //        1.3 Kontrast og lysstyrke
+ //        1.4 pH kalibrering (aktiveres via LCD-meny)
+ //            1.4.1 Les av pH 4 kalibreringsløsning
+ //            1.4.2 Les av pH 10 kalibreringsløsning
+ //            1.4.3 Les av pH 7 kalibreringsløsning
+ //            1.4.4 Finn avvik i span ved hjelp av avleste verdier, og bruk dette til å avgjøre om probe skal skiftes 
+ //            1.4.5 Kalibrer til pH 4
+ //            1.4.6 Kalibrer til pH 10
+ //            1.4.7 Kalibrer til pH 7
+ //        1.5 Reset check pH probe
+ //        1.6 reset check temp probe
+ //        1.7 Annet
+ 
+ // 2. Se om det er gått lang nok tid til at ting skal oppdateres (1000 ms). Hvis så, oppdater alt
+ currentMillis = millis();
+ if(currentMillis >= (lastCurrentMillis + 1000)) {
+ //      2.1 Avles klokke
+ //      2.2 Avles temp og send denne til pH dings hvis endringen fra forrige gang er stor nok f.eks 1 grad. Hvis probefeil, gå til alarmfunksjon med alarmtype, og bli der inntil reset.
+ //      2.3 Avles pH verdi. Hvis probefeil, gå til alarmfunksjon og bli der inntil reset. Limp home modus for pH fra lagrede verdier.
+ //      2.4 Osv
+ 
+ 
+ 
+ // 3. Sett verdier
+ //      3.1 Sett pH output v.h.a. PID
+ //      3.2 pH Max
+ //      3.3 pH Min
+ //      3.4 RSSI
+ //      3.5 Lagre verdi for hvordan pH skal håndteres i tilfellet probefeil. F.eks % on i løpet av siste 24 timer...? Lagres i eprom og oppdateres kun hvis endrinen er vesentlig.
+ 
+ // 4. Send data
+ //     4.1 Send til server/logger over hardware serial
+ //     4.2 Oppdater lokalt display
+ //         4.2.1 pH verdi
+ //         4.2.2 RSSI
+ //         4.2.3 PID output, on eller off
+ //         4.2.4 Max pH
+ //         4.2.5 Min pH
+ //         4.2.6 Tidspunkt for max pH
+ //         4.2.7 Tidspunkt for min pH
+ //         4.2.8 Neste kalibreringsdato
+ //         4.2.9 pH settpunkt
+ //         4.2.10 Temp
+ 
+ 
+ lastCurrentMillis = currentMillis;
+ } // End 1000 ms loop
+ 
  
   
     if (input_stringcomplete){                                                 //if a string from the PC has been recived in its entierty 
@@ -140,11 +199,9 @@ void loop() {
       }
   
   
-void setTime(){
-  // Finn og sett tiden i diverse statiske variabler
-  
- 
-  }
+
+
+
 
 DateTime now = RTC.now();
 Serial.print(now.year(), DEC);
@@ -178,6 +235,9 @@ Serial.println();
 //Serial.println(__TIME__);
 Serial.println();
 
+readRTC();
+Serial.println(nowSecond, DEC);
+
  // set the cursor to column 0, line 1. (note: line 1 is the second row, since counting begins with 0):
  // lcd.setCursor(0, 1);
   // print the number of seconds since reset:
@@ -207,5 +267,20 @@ delay(10
 );
 
 
-}
+} // End loop
+
+/////////////////////////////////////////////////////////
+//     Functions
+/////////////////////////////////////////////////////////
+
+void readRTC(){
+  // Finn og sett tiden i diverse statiske variabler
+  DateTime now = RTC.now();
+  nowYear = now.year();
+  nowMonth = now.month();
+  nowDay = now.day();
+  nowHour = now.hour();
+  nowMinute = now.minute();
+  nowSecond = now.second(); 
+  } // End readRTC
 
